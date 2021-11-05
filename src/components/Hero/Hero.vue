98792823a21e111ac5df93b67feaf7649a3ec0ee
Alex Shyba <alex.shyba@uniform.dev>
2021-11-05 11:13:15 -0700

    feat: use RoutableSitecoreLink

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -24,7 +24,14 @@
           <rect width="404" height="784" fill="url(#svg-pattern-squares-1)" />
         </svg>
         <svg
-          class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+          class="
+            absolute
+            left-full
+            transform
+            -translate-y-3/4 -translate-x-1/4
+            md:-translate-y-1/2
+            lg:-translate-x-1/2
+          "
           width="404"
           height="784"
           fill="none"
@@ -52,36 +59,91 @@
       <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
         <div class="text-center">
           <h2
-            class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+            class="
+              text-4xl
+              tracking-tight
+              leading-10
+              font-extrabold
+              text-gray-900
+              sm:text-5xl sm:leading-none
+              md:text-6xl
+            "
           >
             <sc-text :field="fields.title" />
             <br class="xl:hidden" />
             <sc-text :field="fields.subtitle" tag="span" class="text-red-500" />
           </h2>
           <p
-            class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+            class="
+              mt-3
+              max-w-md
+              mx-auto
+              text-base text-gray-500
+              sm:text-lg
+              md:mt-5 md:text-xl md:max-w-3xl
+            "
           >
             <sc-text :field="fields.text" />
           </p>
           <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
             <div v-if="primaryCTA.title && primaryCTA.link" class="rounded-md shadow">
-              <sc-link
+              <RoutableSitecoreLink
                 :field="fields.primaryCTALink"
-                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-500 hover:bg-red-500 focus:outline-none focus:shadow-outline-red transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                class="
+                  w-full
+                  flex
+                  items-center
+                  justify-center
+                  px-8
+                  py-3
+                  border border-transparent
+                  text-base
+                  leading-6
+                  font-medium
+                  rounded-md
+                  text-white
+                  bg-red-500
+                  hover:bg-red-500
+                  focus:outline-none focus:shadow-outline-red
+                  transition
+                  duration-150
+                  ease-in-out
+                  md:py-4 md:text-lg md:px-10
+                "
               >
                 <sc-text :field="fields.primaryCTATitle" />
-              </sc-link>
+              </RoutableSitecoreLink>
             </div>
             <div
               v-if="secondaryCTA.title && secondaryCTA.link"
               class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3"
             >
-              <sc-link
+              <RoutableSitecoreLink
                 :field="fields.secondaryCTALink"
-                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-red-500 bg-white hover:text-red-500 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                class="
+                  w-full
+                  flex
+                  items-center
+                  justify-center
+                  px-8
+                  py-3
+                  border border-transparent
+                  text-base
+                  leading-6
+                  font-medium
+                  rounded-md
+                  text-red-500
+                  bg-white
+                  hover:text-red-500
+                  focus:outline-none focus:shadow-outline-blue
+                  transition
+                  duration-150
+                  ease-in-out
+                  md:py-4 md:text-lg md:px-10
+                "
               >
                 <sc-text :field="fields.secondaryCTATitle" />
-              </sc-link>
+              </RoutableSitecoreLink>
             </div>
           </div>
         </div>
@@ -94,6 +156,7 @@
 import { Text, Link } from '@sitecore-jss/sitecore-jss-vue';
 import MenuDesktop from './MenuDesktop';
 import MenuMobile from './MenuMobile';
+import RoutableSitecoreLink from '../RoutableSitecoreLink/RoutableSitecoreLink';
 
 export default {
   components: {
@@ -101,6 +164,7 @@ export default {
     MenuMobile,
     ScText: Text,
     ScLink: Link,
+    RoutableSitecoreLink,
   },
   data() {
     return {
